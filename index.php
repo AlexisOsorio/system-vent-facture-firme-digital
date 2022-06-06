aa96eae50b40604a8c15a5592fa1c42bd3d1a266
Alexiss <kevinosorio201@gmail.com>
2022-06-05 23:32:50 -0500

    login y all min

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,12 +12,40 @@
     <link rel="stylesheet" href="utils/css/css/all.min.css">
     <link rel="stylesheet" href="utils/css/style.css">
 </head>
+
 <body>
     <img class="wave" src="utils/images/ola.png" alt="img1">
     <div class="contenedor">
         <div class="img">
             <img src="utils/images/fondo.svg" alt="">
         </div>
+        <div class="contenido-login">
+            <form action="" method="post">
+                <img src="utils/images/user.png" alt="Usuario">
+                <h2>Log In System</h2>
+                <div class="input-div usr">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Usuario</h5>
+                        <input type="text" name="usuario" placeholder="Ingese su usuario" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                        <div class="i">
+                            <i class="fas fa-lock"></i>
+                            <!--colocamos el icono de contraseña-->
+                        </div>
+                        <div class="div">
+                            <h5>Contraseña</h5>
+                            <input type<input type="password" name="pass" placeholder="Ingrese su contraseña" class="input">
+                        </div>
+                    </div>
+                    <input type="submit" class="btn" value="Iniciar Sesión">
+            </form>
+        </div>
     </div>
 </body>
+
 </html>
