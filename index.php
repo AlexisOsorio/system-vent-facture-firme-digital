33fc73a812559f4d791fdf52fd01a0c4d447e8b4
Alexiss <kevinosorio201@gmail.com>
2022-06-05 23:20:58 -0500

    modificando el login y agregando images, fonts, css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | System Billing</title>
+    <!--Fuente-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <!--Estilos CSS-->
+    <link rel="stylesheet" href="utils/css/css/all.min.css">
+    <link rel="stylesheet" href="utils/css/style.css">
+</head>
+<body>
+    <img class="wave" src="utils/images/ola.png" alt="img1">
+    <div class="contenedor">
+        <div class="img">
+            <img src="utils/images/fondo.svg" alt="">
+        </div>
+    </div>
+</body>
+</html>
