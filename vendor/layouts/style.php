5e6a003605a01745bfcf0b3da2725b81914a9812
Alexiss <kevinosorio201@gmail.com>
2022-06-10 22:26:08 -0500

    crear funciones de horario

--- vendor/layouts/style.php
+++ vendor/layouts/style.php
@@ -1,3 +1,5 @@
 <link rel="stylesheet" type="text/css" href="utils/css/styless.css">
-<script type="text/js" src="utils/js/functions.js"></script>
-<script src="../core/functions.php"></script>
+<script type="text/javascript" src="utils/js/functions.js"></script>
+<?php 
+include "functions.php";
+?>
