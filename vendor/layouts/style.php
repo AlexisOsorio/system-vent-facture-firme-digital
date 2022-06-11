9865dfb6c827a5cc1d88806f5690ab00cd894e52
Alexiss <kevinosorio201@gmail.com>
2022-06-11 00:10:51 -0500

    registro y nuevas librerias

--- vendor/layouts/style.php
+++ vendor/layouts/style.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" type="text/css" href="../utils/css/styless.css">
+<link rel="stylesheet" type="text/css" href="../utils/css/bootstrap.min.css">
+<script type="text/javascript" src="../utils/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../utils/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="../utils/js/functions.js"></script>
 <?php 
 include "functions.php";
--- vendor/views/registro_users.php
+++ vendor/views/registro_users.php
@@ -21,6 +21,19 @@
             <form action="">
                 <label for="nombre">Nombre:</label>
                 <input type="text" name="nombre" id="nombre" placeholder="Nombre Completo">
+				<label for="correo">Correo Electrónico</label>
+				<input type="email" name="correo" id="correo" placeholder="Correo Electrónico">
+				<label for="usuario">Usuario:</label>
+                <input type="text" name="usuario" id="usuario" placeholder="Ingrese el Usuario">
+				<label for="pass">Contraseña:</label>
+                <input type="password" name="pass" id="pass" placeholder="Ingrese su Contraseña">
+				<label for="rol">Tipo de Usuario:</label>
+				<select name="rol" id="rol">
+					<option value="1">Administrador</option>
+					<option value="2">Supervisor</option>
+					<option value="3">Vendedor</option>
+				</select>
+				<input type="submit" value="Registrar Usuario" class="btn btn">
             </form>
         </div>
 	</section>
