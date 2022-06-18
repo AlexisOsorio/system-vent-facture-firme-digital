89705c3ee90631a8c20c0ac0439a8e9a1569e2f9
Kevin Osorio <kevinosorio201@gmail.com>
2022-06-18 00:04:48 -0500

    lista clientes

--- vendor/views/list_clients.php
+++ vendor/views/list_clients.php
@@ -13,7 +13,7 @@ include_once "../../config/conexion.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lista de usuarios</title>
+    <title>Lista de Clientes</title>
 
     <?php
     include_once "../layouts/style.php"
@@ -43,12 +43,12 @@ include_once "../../config/conexion.php";
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h2><b>Lista Usuarios</b></h2>
+                        <h2><b>Lista Clientes</b></h2>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="../views/index.php">Inicio</a></li>
-                            <li class="breadcrumb-item active">Lista Usuarios</li>
+                            <li class="breadcrumb-item active">Lista Clientes</li>
                         </ol>
                     </div>
                 </div>
@@ -69,7 +69,7 @@ include_once "../../config/conexion.php";
 
                                 <ul class="nav justify-content-end">
                                     <li class="nav-item">
-                                        <a href="../views/registro_users.php" class=" btn bg-primary">Crear Usuario</a>
+                                        <a href="../views/registro_clients.php" class=" btn bg-primary">Nuevo Ciente</a>
                                     </li>
                                 </ul>
                             </div>
@@ -80,16 +80,15 @@ include_once "../../config/conexion.php";
                                     <tr class="text-center">
                                         <th scope="col-sm-2">ID</th>
                                         <th scope="col-sm-2">NOMBRE</th>
-                                        <th scope="col-sm-2">CORREO</th>
-                                        <th scope="col-sm-2">USUARIO</th>
-                                        <th scope="col-sm-2">ROL</th>
+                                        <th scope="col-sm-2">TELÉFONO</th>
+                                        <th scope="col-sm-2">DIRECCIÓN</th>
                                         <th scope="col-sm-2">ACCIONES</th>
                                     </tr>
                                 </thead>
                                 <?php
 
                                 //paginador
-                                $sql_reg =  mysqli_query($conexion, "SELECT COUNT(*) as registros_totales FROM usuario WHERE estatus = 1");
+                                $sql_reg =  mysqli_query($conexion, "SELECT COUNT(*) as registros_totales FROM cliente WHERE estatus = 1");
                                 $result_reg = mysqli_fetch_array($sql_reg);
                                 $registros_totales = $result_reg['registros_totales'];
 
@@ -104,25 +103,24 @@ include_once "../../config/conexion.php";
                                 $desde_pg = ($pag - 1) * $pag_num;
                                 $total_pg = ceil($registros_totales / $pag_num);
 
-                                $query = mysqli_query($conexion, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u 
-                                    INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY u.idusuario ASC LIMIT $desde_pg,$pag_num ");
+                                $query = mysqli_query($conexion, "SELECT c.idcliente, c.nombre, c.telefono, c.direccion FROM cliente c 
+                                    WHERE estatus = 1 ORDER BY c.idcliente ASC LIMIT $desde_pg,$pag_num ");
                                 $result = mysqli_num_rows($query);
                                 if ($result > 0) {
                                     while ($data = mysqli_fetch_array($query)) {
                                 ?>
                                         <tbody>
                                             <tr class="text-center">
-                                                <th scope="row"><?php echo $data['idusuario']; ?></th>
+                                                <th scope="row"><?php echo $data['idcliente']; ?></th>
                                                 <td><?php echo $data['nombre']; ?></td>
-                                                <td><?php echo $data['correo']; ?></td>
-                                                <td><?php echo $data['usuario']; ?></td>
-                                                <td><?php echo $data['rol']; ?></td>
+                                                <td><?php echo $data['telefono']; ?></td>
+                                                <td><?php echo $data['direccion']; ?></td>
                                                 <td>
-                                                    <a href="editar_usuarios.php?id=<?php echo $data['idusuario']; ?>" class="btn bg-warning"><i class="nav-icon fas fa-edit"></i> Editar Usuario</a>
+                                                    <a href="editar_clients.php" class="btn bg-warning"><i class="nav-icon fas fa-edit"></i> Editar Cliente</a>
                                                     <?php
-                                                    if ($data['idusuario'] != 1) {
+                                                    if ($data['idcliente'] != 1) {
                                                     ?>
-                                                        <a href="delete_users.php?id=<?php echo $data['idusuario']; ?>" class="btn bg-danger"><i class="nav-icon fas fa-trash"></i> Eliminar Usuario</a>
+                                                        <a href="delete_clients.php" class="btn bg-danger"><i class="nav-icon fas fa-trash"></i> Eliminar Cliente</a>
                                                     <?php
                                                     }
                                                     ?>
